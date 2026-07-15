95f4befeb1e4acd0b7f37151bfa49151683d8109
Jean <jean.jarbas@gmail.com>
2026-07-15 12:13:29 -0300

    feat: adiciona entrada de terminal e validações de erros

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { pool, testDatabaseConnection } from "./database/connection.js";
+import { ConsoleInput } from "./utils/ConsoleInput.js";
 
 async function main(): Promise<void> {
   console.log("Bookstore Manager CLI");
@@ -7,6 +8,15 @@ async function main(): Promise<void> {
     console.log("Conexão com a base de dados realizada.");
   } finally {
     await pool.end();
+  }
+
+  const input = new ConsoleInput();
+
+  try {
+    const nome = await input.askRequired("Nome: ");
+    console.log(nome);
+  } finally {
+    input.close();
   }
 }
 
