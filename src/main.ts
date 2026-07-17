c7ad6953bffea54d4dd0b3f9bda9e3c18b15258a
Jean <jean.jarbas@gmail.com>
2026-07-16 22:26:50 -0300

    feat: adiciona gerenciamento de livros

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import { pool, testDatabaseConnection } from "./database/connection.js";
-import { ConsoleInput } from "./utils/ConsoleInput.js";
-import { AutorRepository } from "./repositories/AutorRepository.js";
-import { AutorService } from "./services/AutorService.js";
 import { AutorController } from "./controllers/AutorController.js";
-import { MainMenu } from "./menus/MainMenu.js";
-import { getFriendlyErrorMessage } from "./utils/AppError.js";
+import { LivroController } from "./controllers/LivroController.js";
 import { ClienteController } from "./controllers/ClienteController.js";
+import { pool, testDatabaseConnection } from "./database/connection.js";
+import { MainMenu } from "./menus/MainMenu.js";
+import { AutorRepository } from "./repositories/AutorRepository.js";
+import { LivroRepository } from "./repositories/LivroRepository.js";
 import { ClienteRepository } from "./repositories/ClienteRepository.js";
+
+import { AutorService } from "./services/AutorService.js";
+import { LivroService } from "./services/LivroService.js";
 import { ClienteService } from "./services/ClienteService.js";
+
+import { ConsoleInput } from "./utils/ConsoleInput.js";
+import { getFriendlyErrorMessage } from "./utils/AppError.js";
 
 async function main(): Promise<void> {
   console.log("Bookstore Manager CLI");
@@ -18,14 +23,22 @@ async function main(): Promise<void> {
     console.log("Conexão com o PostgreSQL realizada.");
 
     const authorRepository = new AutorRepository();
+    const bookRepository = new LivroRepository();
     const clientRepository = new ClienteRepository();
 
     const authorService = new AutorService(authorRepository);
+    const bookService = new LivroService(bookRepository, authorRepository);
     const clientService = new ClienteService(clientRepository);
 
     const authorController = new AutorController(authorService, input);
+    const bookController = new LivroController(bookService, input);
     const clientController = new ClienteController(clientService, input);
-    const mainMenu = new MainMenu(input, authorController, clientController);
+    const mainMenu = new MainMenu(
+      input,
+      authorController,
+      bookController,
+      clientController,
+    );
 
     await mainMenu.run();
     console.log("Sessão encerrada. Até logo!");
